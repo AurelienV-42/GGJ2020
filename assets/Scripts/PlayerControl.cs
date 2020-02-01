5093b5c1c66d722076a9c02e500ff56b6e7ad675
Aurélien <aurelien1999@msn.com>
2020-02-01 16:51:25 +0100

    [+] In-GAME : Movements and Jump

---
+++ assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public float jumpSpeed = 15f;
+    public float speed = 5f;
+    private bool onGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if ((Input.GetMouseButton(0) || Input.GetKey(KeyCode.UpArrow)) && onGround)
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+    }
+}
