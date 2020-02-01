c292ba13f269bb6e2178393ef0a5db476c63077f
Aurélien <aurelien1999@msn.com>
2020-02-01 19:49:14 +0100

    [+] Force test for movements

--- assets/Scripts/PlayerControl.cs
+++ assets/Scripts/PlayerControl.cs
@@ -4,29 +4,51 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private bool onGround;
+    private float x_movement;
+
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     public float jumpSpeed = 5f;
-    public float speed = 4f;
-    private bool onGround;
+    public float max_speed = 4f;
+    public float movement_scalar;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        jump();
+        
+        // WITH A FORCE 
+        x_movement = Input.GetAxis("Horizontal");
 
-        if ((Input.GetMouseButton(0) || Input.GetKey(KeyCode.UpArrow)) && onGround)
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        
+        if (rb.velocity.magnitude < max_speed)
+        {
+            Vector2 movement = new Vector2(x_movement, 0);
+            rb.AddForce(movement_scalar * movement);
+        }
+
+        // WITH VELOCITY
+        /*
         if (Input.GetKey(KeyCode.RightArrow))
             rb.velocity = new Vector2(speed, rb.velocity.y);
         if (Input.GetKey(KeyCode.LeftArrow))
             rb.velocity = new Vector2(-speed, rb.velocity.y);
+        */
     }
+    private void jump()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(KeyCode.UpArrow) && onGround)
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+    }
+
 }
